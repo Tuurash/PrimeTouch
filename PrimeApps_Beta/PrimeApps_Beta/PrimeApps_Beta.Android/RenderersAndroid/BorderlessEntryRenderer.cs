2cd249dcf1b80ee2a5d37a28bc96f7aa5448cd01
Tuurash <turasmimoo@gmail.com>
2021-01-12 15:08:38 +0600

    init

---
+++ PrimeApps_Beta/PrimeApps_Beta/PrimeApps_Beta.Android/RenderersAndroid/BorderlessEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using PrimeApps_Beta.Droid.RenderersAndroid;
+using PrimeApps_Beta.Renderers;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessBorderlessEntryRenderer))]
+namespace PrimeApps_Beta.Droid.RenderersAndroid
+{
+    public class BorderlessBorderlessEntryRenderer : EntryRenderer
+    {
+        public BorderlessBorderlessEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
